bc39d4205a1d8043d9bcdb3a7295308f048d190e
Krasiyan <hello@krasiyan.com>
2017-01-31 00:12:16 +0200

    imported the organic stem skeleton
    added the 'mocha-chai' stack upgrade
    
    added the 'mongoose' stack upgrade
    
    added the 'devtools' stack upgrade

---
+++ index.js
@@ -0,0 +1,2 @@
+process.env.CELL_MODE = process.env.CELL_MODE || '_development'
+require('./server/start')()
---
+++ scripts/develop.js
@@ -0,0 +1,19 @@
+module.exports = function (angel) {
+  angel.on('develop', function (angel, next) {
+    process.env.CELL_MODE = process.env.CELL_MODE || '_development'
+    var parallel = require('organic-stem-devtools/lib/parallel-exec')
+    parallel([
+      'node ./node_modules/.bin/angel watch',
+      'node ./index.js'
+    ])
+  })
+
+  angel.on('develop :part', function (angel, next) {
+    process.env.CELL_MODE = process.env.CELL_MODE || '_development'
+    var parallel = require('organic-stem-devtools/lib/parallel-exec')
+    parallel([
+      'node ./node_modules/.bin/angel watch "' + angel.cmdData.part + '"',
+      'node ./index.js'
+    ])
+  })
+}
