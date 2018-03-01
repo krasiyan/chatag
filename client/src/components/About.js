520dccbe155548326a324cf8030151ce4805b9ad
Krasiyan Nedelchev <hello@krasiyan.com>
2018-03-01 18:59:34 +0200

    minor about page layout fix

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 class About extends Component {
   render () {
     return (
-      <div>
-        about
+      <div className="container">
+        <h3>About</h3>
       </div>
     )
   }
