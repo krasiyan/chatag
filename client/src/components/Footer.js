0f81577d76b8ebe59c99fe2389eb6b64cb4ba6a9
Krasiyan Nedelchev <hello@krasiyan.com>
2018-03-01 18:15:21 +0200

    minor typo fix

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import FontAwesome from 'react-fontawesome';
 import $ from 'jquery';
 
-class Navbar extends Component {
+class Footer extends Component {
   componentDidMount () {
     $(".button-collapse").sideNav();
   }
