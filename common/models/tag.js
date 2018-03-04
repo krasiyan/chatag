f41459fbda5075e34ae6a33a43e39ee5c5c8dc57
Krasiyan Nedelchev <hello@krasiyan.com>
2018-03-04 19:33:33 +0200

    wip expose the 'destoryAll' remote method via the 'DELETE /api/tags' API endpoint

--- common/models/tag.js
+++ common/models/tag.js
@@ -1,5 +1,11 @@
 'use strict';
 
 module.exports = function(Tag) {
-
+  Tag.remoteMethod('destroyAll', {
+    isStatic: true,
+    description: 'Delete all matching records',
+    accessType: 'WRITE',
+    accepts: {arg: 'where', type: 'object', description: 'filter.where object'},
+    http: {verb: 'del', path: '/'}
+  });
 };
