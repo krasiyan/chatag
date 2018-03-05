b3601ab05722fb094a9f27c461e34ceb916fbc5f
Krasiyan Nedelchev <hello@krasiyan.com>
2018-03-05 12:36:02 +0200

    expose the 'destroyAll' Tag model remote method only if the 'exposeDestroyAllTagRemoteMethod' configuration variable is set
    set 'exposeDestroyAllTagRemoteMethod' only for the 'development' node env

--- common/models/tag.js
+++ common/models/tag.js
@@ -1,11 +1,15 @@
 'use strict';
 
-module.exports = function(Tag) {
-  Tag.remoteMethod('destroyAll', {
-    isStatic: true,
-    description: 'Delete all matching records',
-    accessType: 'WRITE',
-    accepts: {arg: 'where', type: 'object', description: 'filter.where object'},
-    http: {verb: 'del', path: '/'},
-  });
+var app = require('../../server/server')
+
+module.exports = function (Tag) {
+  if (app.get('exposeDestroyAllTagRemoteMethod')) {
+    Tag.remoteMethod('destroyAll', {
+      isStatic: true,
+      description: 'Delete all matching records',
+      accessType: 'WRITE',
+      accepts: {arg: 'where', type: 'object', description: 'filter.where object'},
+      http: {verb: 'del', path: '/'},
+    });
+  }
 };
