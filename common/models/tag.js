b29948a9062bb1c97e1c5bbc537c05b86b999982
Krasiyan Nedelchev <hello@krasiyan.com>
2018-03-14 23:06:33 +0200

    wip emitting WS events in the 'Tag' model save hooks
    'tagUpdated' when an exisitng tag entry is modified
    
    'tagCreated' when a new tag entry is created
    
    'tagDeleted' when an exisitng tag entry is removed

--- common/models/tag.js
+++ common/models/tag.js
@@ -16,4 +16,17 @@ module.exports = function(Tag) {
       http: {verb: 'del', path: '/'},
     });
   }
+
+  Tag.observe('after save', function (ctx, next) {
+    var eventName = ctx.isNewInstance ? 'tagCreated' : 'tagUpdated'
+    Order.app.io.emit(eventName, ctx.instance);
+
+    next();
+  });
+
+  Tag.observe('before delete', function (ctx, next) {
+    Order.app.io.emit('tagDeleted', { id: ctx.instance.id });
+
+    next();
+  })
 };
