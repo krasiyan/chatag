7eef1c3501e577dcd486d622ed7fc8d58b85384a
Krasiyan Nedelchev <hello@krasiyan.com>
2018-04-07 19:14:04 +0300

    change the server build path to 'build/server'

--- backpack.config.js
+++ backpack.config.js
@@ -4,7 +4,9 @@ const path = require('path');
 
 module.exports = {
   webpack: (config, options, webpack) => {
-    config.entry.main = './server/server.js'
+    config.entry.main = './server/server.js';
+    config.output.path = path.join(process.cwd(), 'build/server');
+
     return config;
   },
 };
